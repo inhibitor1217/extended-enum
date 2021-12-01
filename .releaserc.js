086be7a64485a3b171dc97351efd5f19a64027a4
dugi <dugi@channel.io>
2021-12-01 22:52:54 +0900

    chore(semantic-release): setup configuration

---
+++ .releaserc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', {
+      'tarballDir': 'release'
+    }],
+    ['@semantic-release/github', {
+      assets: 'release/*.tgz'
+    }],
+    '@semantic-release/git'
+  ]
+};
