380f2886f5294f7502de35b6841a009766fc0bb7
dugi <dugi@channel.io>
2021-12-02 16:03:19 +0900

    feat(type): define ExtendedEnum, ExtendedEnumStatic interfaces

--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,28 @@
-export type Enum = Record<string, string | number>;
+type Primitive = string | number;
+
+export type Enum = Record<string, Primitive>;
 
 export type Keys<E extends Enum> = keyof E;
+
+export interface ExtendedEnum<V extends Primitive> {
+  is(value: V): boolean;
+  is(value: Primitive): boolean;
+  is(value: ExtendedEnum<V>): boolean;
+
+  valueOf(): Primitive;
+
+  toString(): string;
+}
+
+export interface ExtendedEnumStatic<E extends Enum, V extends Primitive>
+  extends Iterable<ExtendedEnum<V>> {
+  of(value: V): ExtendedEnum<V>;
+
+  from(value: string | number): ExtendedEnum<V> | undefined;
+  from(value: string | number, fallback: V): ExtendedEnum<V>;
+
+  keys(): Iterable<Keys<E>>;
+  values(): Iterable<ExtendedEnum<V>>;
+  rawValues(): Iterable<V>;
+  entries(): Iterable<[Keys<E>, ExtendedEnum<V>]>;
+}
